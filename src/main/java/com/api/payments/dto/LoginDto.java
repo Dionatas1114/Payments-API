d8d2cefb432a88b408cb503e39e41651d5fff9ac
Diônatas Lazzari <sem.dionatas@gmail.com>
2023-03-26 23:59:34 -0300

    feat: add LoginDto

---
+++ src/main/java/com/api/payments/dto/LoginDto.java
@@ -0,0 +1,16 @@
+package com.api.payments.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class LoginDto {
+
+    public String email;
+    public String password;
+}
