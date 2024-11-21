89f85f367783c184166ce0ad8df4a46e36b8ba70
Dionatas1114 <sem.dionatas@gmail.com>
2024-11-21 09:50:19 -0300

    test

--- src/main/java/com/api/payments/config/SecurityConfig.java
+++ src/main/java/com/api/payments/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.api.payments.interceptor.TokenInterceptor;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +21,14 @@ public class SecurityConfig implements WebMvcConfigurer {
         registry.addInterceptor(tokenInterceptor)
                 .addPathPatterns(properties.getSecurePaths()) // add list of protected routes
                 .excludePathPatterns(properties.getPublicPaths()); // add list of public routes (ex. login page)
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // authorized domains //TODO move to env var
+                .allowedMethods("GET", "POST", "PUT", "DELETE") //TODO move to env var
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
