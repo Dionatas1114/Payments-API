d63b5bdcf9a6dcc367cb72128a1ae285117bb9e9
pc <sem.dionatas@gmail.com>
2025-02-22 22:37:41 -0300

    fix uuid

--- src/main/java/com/api/payments/entity/TransactionBaseEntity.java
+++ src/main/java/com/api/payments/entity/TransactionBaseEntity.java
@@ -1,6 +1,8 @@
 package com.api.payments.entity;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.JoinColumn;
@@ -10,6 +12,8 @@ import java.time.LocalDate;
 import java.util.Objects;
 
 @MappedSuperclass
+@Setter
+@Getter
 public class TransactionBaseEntity extends BaseEntity {
 
     @ApiModelProperty(notes = "Nome Completo do Devedor")
@@ -70,145 +74,13 @@ public class TransactionBaseEntity extends BaseEntity {
     public String messageText = "None";
 
     @ApiModelProperty(notes = "Usuário da Transação")
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false, columnDefinition = "BINARY(16)")
     @ManyToOne
     private Users user;
-
-    public String getDebtorFullName() {
-        return debtorFullName;
-    }
-
-    public void setDebtorFullName(String debtorFullName) {
-        this.debtorFullName = debtorFullName;
-    }
-
-    public String getDebtorLastName() {
-        return debtorLastName;
-    }
-
-    public void setDebtorLastName(String debtorLastName) {
-        this.debtorLastName = debtorLastName;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public Boolean getPaymentStatus() {
-        return paymentStatus;
-    }
-
-    public void setPaymentStatus(Boolean paymentStatus) {
-        this.paymentStatus = paymentStatus;
-    }
-
-    public LocalDate getExpirationDate() {
-        return expirationDate;
-    }
-
-    public void setExpirationDate(LocalDate expirationDate) {
-        this.expirationDate = expirationDate;
-    }
-
-    public LocalDate getPaymentDate() {
-        return paymentDate;
-    }
-
-    public void setPaymentDate(LocalDate paymentDate) {
-        this.paymentDate = paymentDate;
-    }
-
-    public String getTransactionFrequency() {
-        return transactionFrequency;
-    }
 
     public void setTransactionFrequency(String transactionFrequency) {
         this.transactionFrequency = !(transactionFrequency == null
                 | Objects.equals(transactionFrequency, ""))
                 ? transactionFrequency : "unique";
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public double getInterest() {
-        return interest;
-    }
-
-    public void setInterest(double interest) {
-        this.interest = interest;
-    }
-
-    public double getFine() {
-        return fine;
-    }
-
-    public void setFine(double fine) {
-        this.fine = fine;
-    }
-
-    public double getIncreasedValue() {
-        return increasedValue;
-    }
-
-    public void setIncreasedValue(double increasedValue) {
-        this.increasedValue = increasedValue;
-    }
-
-    public double getDiscPayAdvance() {
-        return discPayAdvance;
-    }
-
-    public void setDiscPayAdvance(double discPayAdvance) {
-        this.discPayAdvance = discPayAdvance;
-    }
-
-    public double getOriginalValue() {
-        return originalValue;
-    }
-
-    public void setOriginalValue(double originalValue) {
-        this.originalValue = originalValue;
-    }
-
-    public double getTotal() {
-        return total;
-    }
-
-    public void setTotal(double total) {
-        this.total = total;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getMessageText() {
-        return messageText;
-    }
-
-    public void setMessageText(String messageText) {
-        this.messageText = messageText;
-    }
-
-    public Users getUser() {
-        return user;
-    }
-
-    public void setUser(Users user) {
-        this.user = user;
     }
 }
